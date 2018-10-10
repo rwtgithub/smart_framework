6166d8191b3fc628a0462f566ab17a7fb429fef2
rwtgithub <1019908255@qq.com>
2018-10-10 21:09:30 +0800

    10月10号

--- SmartFramework/src/main/java/com/rwt/smartframework/util/JSONUtil.java
+++ SmartFramework/src/main/java/com/rwt/smartframework/util/JSONUtil.java
@@ -1,4 +1,12 @@
 package com.rwt.smartframework.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JSONUtil {
+
+     private static  final Logger logger =LoggerFactory.getLogger(JSONUtil.class);
+
+   
+
 }
